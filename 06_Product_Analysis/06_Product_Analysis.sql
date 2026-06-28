f0e3f848b0ff0c7e27a4683f2d250e17fa94c21e
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-06-29 02:29:13 +0530

    Update 06_Product_Analysis.sql

--- 06_Product_Analysis/06_Product_Analysis.sql
+++ 06_Product_Analysis/06_Product_Analysis.sql
@@ -1,1 +1,43 @@
+---PRODUCT ANALYSIS
+
+---Top 10 products by revenue
+--- To see which products generate more revenue
+
+select top 10
+p.ProductName, sum(od.sales) as revenue
+from OrderDetails od
+join products p on od.ProductID = p.ProductID
+group by p.ProductName
+order by revenue desc;
+
+---Top 10 products by profit
+---Which product actually brings money
+
+select top 10
+p.ProductName, sum(od.Profit) as Profit
+from OrderDetails od
+join products p on od.ProductID = p.ProductID
+group by p.ProductName
+order by Profit desc;
+
+---Bottom 10 products by profit
+--- these products are profit drainers 
+
+select top 10
+p.ProductName, sum(od.Profit) as Profit
+from OrderDetails od
+join products p on od.ProductID = p.ProductID
+group by p.ProductName
+order by Profit asc;
+
+---Profit Margin
+select top 10
+p.ProductName, sum(od.Profit) as profit,
+sum(od.sales) as revenue,
+coalesce(
+sum(od.Profit)*100 / sum(od.sales),0) as Profitmargin
+from Orderdetails od
+join Products p on od.ProductID = p.ProductID
+group by ProductName
+order by ProfitMargin asc;
 
