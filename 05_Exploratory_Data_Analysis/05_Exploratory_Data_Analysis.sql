4aae1e54c49b5f46d07caae11698c66ea2639b7e
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-06-29 02:23:33 +0530

    Update 05_Exploratory_Data_Analysis.sql

--- 05_Exploratory_Data_Analysis/05_Exploratory_Data_Analysis.sql
+++ 05_Exploratory_Data_Analysis/05_Exploratory_Data_Analysis.sql
@@ -1,1 +1,49 @@
+--- Total Revenue
 
+select sum(sales) as total_rev
+from orderdetails
+
+-- total profit
+select sum(profit) as tot_profit
+from OrderDetails
+
+--Total orders
+select count(*) as order_count
+from orders
+
+
+--Total customers
+select count(*) as customer_count
+from customers
+
+--Total products
+select count(*)
+from products
+
+--Avg orde value                                   ---- avg orde value = sum of sales / total orders  or total rev/ orders (how much money is a customer spending per order)
+select sum(sales) *1.0/
+count(distinct OrderID) as avgorder_value
+from OrderDetails
+
+--project margin                                  ---- profit margin =(total profit ÷ total revenue) × 100
+select
+round(
+sum(Profit) * 100.0 /
+sum(Sales)
+,2) as ProfitMargin
+from OrderDetails;
+
+select distinct DeliveryStatus
+from Shipping;
+
+select distinct ShippingMode
+from Shipping;
+
+select distinct Latedeliveryrisk
+from Shipping;
+
+SELECT
+min(Daysforshippingreal),
+max(Daysforshippingreal),
+avg(Daysforshippingreal)
+FROM Shipping;
