9b914806d58e0a84ce9c45a361118d6e8823c959
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-06-29 00:01:15 +0530

    Update 01_Data_Import.sql

--- 01_Data_Import/01_Data_Import.sql
+++ 01_Data_Import/01_Data_Import.sql
@@ -1,1 +1,17 @@
+--Create a database
+create database SupplychainDB;
+
+--using this as my current database for querying
+use SupplychainDB
+
+--imported Raw_supplychain_data.csv 
+
+
+--verify import
+select count(*) as TotalRows
+from Raw_supplychain_data;
+
+--preview data
+select TOP 5 *
+from Raw_supplychain_data;
 
