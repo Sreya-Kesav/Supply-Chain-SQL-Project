fcf4aaa9af9551064e6654309172fd3b7399f3b3
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-06-29 02:41:33 +0530

    Create 06A_Product_Analysis_Using_Views.sql

---
+++ 06_Product_Analysis/06A_Product_Analysis_Using_Views.sql
@@ -0,0 +1,36 @@
+---PRODUCT ANALYSIS WITH VIEWS
+
+create view vw_prodcutperformance as
+select 
+p.ProductName, sum(od.sales) as revenue,sum(od.Profit) as Profit
+from OrderDetails od
+join products p on od.ProductID = p.ProductID
+group by p.ProductName
+go
+
+---Top 10 products by revenue
+
+select top 10
+ProductName,revenue
+from vw_prodcutperformance
+order by revenue desc;
+
+---Top 10 products by profit
+select top 10
+ProductName,Profit
+from vw_prodcutperformance
+order by Profit desc;
+
+---Bottom 10 products by profit
+select top 10
+ProductName,Profit
+from vw_prodcutperformance
+order by Profit desc;
+
+---Profit Margin
+select top 10
+ProductName,Profit, revenue,
+coalesce(
+(Profit) *100 / revenue,0) as Profitmargin
+from vw_prodcutperformance
+order by Profitmargin asc;
