e013837145ace3454ee84d9200890a22ff4ab5c8
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-06-29 16:43:46 +0530

    Update 07_Category_Analysis.sql

--- 07_Category_Analysis/07_Category_Analysis.sql
+++ 07_Category_Analysis/07_Category_Analysis.sql
@@ -1,1 +1,44 @@
+---CATEGORY ANALYSIS
+--Top categories by revenue
+
+select top 10
+ca.categoryname, sum(od.sales) as revenue
+from categories ca
+join products p on ca.categoryid = p.categoryid
+join OrderDetails od on p.productid = od.productid
+group by ca.categoryname
+order by revenue desc;
+
+--Top category by profit
+
+select top 10
+ca.categoryname, sum(od.Profit) as profit
+from categories ca
+join products p on ca.categoryid = p.categoryid
+join OrderDetails od on p.productid = od.productid
+group by ca.categoryname
+order by profit desc;
+
+-- profit margin
+
+select top 10
+ca.categoryname, sum(od.Profit) as profit, sum(od.Sales) as revenue,
+coalesce (round(sum(od.Profit)*100.0/ nullif(sum(od.sales),0),2),0) as profit_margin
+from categories ca
+join products p on ca.categoryid = p.categoryid
+join OrderDetails od on p.productid = od.productid
+group by ca.categoryname
+order by profit_margin desc;
+
+---revenue contribution
+---percentage of total company revenue that each category contributes
+
+select top 10
+ca.categoryname, sum(od.sales) as revenue , 
+sum(od.sales)*100/(select sum(sales) from OrderDetails od) as revenue_contribution            ---- revenue contribution = (category rev/ total rev )*100
+from categories ca
+join products p on ca.categoryid = p.categoryid
+join OrderDetails od on p.productid = od.productid
+group by categoryname
+order by revenue_contribution desc; 
 
